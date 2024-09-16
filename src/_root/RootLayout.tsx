aff41bd46da47ab486723bd5bd84f320d66ac769
Ose Ogbe <osememen.ogbe@gmail.com>
2024-09-16 20:09:58 +0100

    left sidebar

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,10 +1,17 @@
+import { Outlet } from "react-router-dom"
 
+import LeftSidebar from "@/components/shared/LeftSidebar"
 import Topbar from "@/components/shared/Topbar"
 
 const RootLayout = () => {
     return (
         <div className="w-full md:flex">
             <Topbar />
+            <LeftSidebar />
+
+            <section className="flex flex-1 h-full">
+                <Outlet />
+            </section>
         </div>
     )
 }
